84662cf24913630d626411155c1087688a8add0c
ckhatri <chirag.k.khatri@gmail.com>
2016-04-18 15:27:21 -0500

    updated script with comments to allow people to better understand what's happening

--- dlWebPage.py
+++ dlWebPage.py
@@ -9,6 +9,7 @@ except ImportError:
 	print "[*] Please download and install Beautiful Soup first!"
 	sys.exit(0)
 
+#gets the url, creates the folder name, and gets the extension
 url = raw_input("[+] Enter the url: ")
 folderName = raw_input("[+] Enter name for folder: ")
 extension = raw_input("[+] Enter the extension you want, with period before it: ")
@@ -17,14 +18,15 @@ i = 0
 request = urllib2.Request(url, None, headers)
 html = urllib2.urlopen(request)
 soup = BeautifulSoup(html.read())
+#make the directory with the folder name in the current folder
 os.mkdir(folderName)
  # find <a> tags with href in it
 for tag in soup.findAll('a', href=True):
 	tag['href'] = urlparse.urljoin(url, tag['href'])
+	#if extension equals the extension we want, download it, put it into the folder.
 	if os.path.splitext(os.path.basename(tag['href']))[1] == extension:
 		current = urllib2.urlopen(tag['href'])
 		print "[*] Downloading: %s" %(os.path.basename(tag['href']))
-
 		f = open(folderName + "/" + os.path.basename(tag['href']), "wb")
 		f.write(current.read())
 		f.close()
